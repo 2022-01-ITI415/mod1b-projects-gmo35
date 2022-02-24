ac8795aeeb79849ef408b4e561e63741d94f3290
gmo35 <gmo35@scarletmail.rutgers.edu>
2022-02-24 00:49:58 -0500

    collectibles
    allows the pickups to be picked up by the projectile2 without it jumping off of it

---
+++ Assets/CollisionScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class CollisionScript : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    
+}
